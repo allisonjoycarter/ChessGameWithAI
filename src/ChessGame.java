c797abea73cbc9fcf9ba598d53ab394c37da7dfc
Unknown <allisonjoy413@gmail.com>
2018-03-03 09:47:09 -0500

    Added classes from Project
    Added code from blackboard and created the Move object

--- src/ChessGame.java
+++ src/ChessGame.java
@@ -2,9 +2,6 @@ public class ChessGame {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Chess!");
-        //I changed this!
-        //making another change
+
     }
-    //and another one
-    ///a change
 }
